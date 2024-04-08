c1d4c3a0bb77611db152098089ffdc7508707de6
Rohan Godha <rgodha24@brophybroncos.org>
2024-04-08 13:44:14 -0700

    i think i did it??

--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,35 @@
 
 use {defmt_rtt as _, panic_probe as _};
 
-fn main() {
+use arduino_nano33iot as bsp;
+use bsp::hal;
+
+
+use bsp::entry;
+use hal::clock::GenericClockController;
+use hal::delay::Delay;
+use hal::pac::{CorePeripherals, Peripherals};
+use hal::prelude::*;
+
+#[entry]
+fn main() -> ! {
+    let mut peripherals = Peripherals::take().unwrap();
+    let core = CorePeripherals::take().unwrap();
+    let mut clocks = GenericClockController::with_internal_32kosc(
+        peripherals.GCLK,
+        &mut peripherals.PM,
+        &mut peripherals.SYSCTRL,
+        &mut peripherals.NVMCTRL,
+    );
+    let pins = bsp::Pins::new(peripherals.PORT);
+    let mut led: bsp::Led = pins.led_sck.into();
+    let mut delay = Delay::new(core.SYST, &mut clocks);
+
+    loop {
+        delay.delay_ms(200u8);
+        led.set_high().unwrap();
+        delay.delay_ms(200u8);
+        led.set_low().unwrap();
+    }
+
 }
